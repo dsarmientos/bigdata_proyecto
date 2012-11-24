b4c7ad018a3c75a973b5b38de8ccaabf01eb9111
Daniel Sarmiento <dsarmientos@gmail.com>
2012-11-23 20:47:51 -0500

    Listo para empezar oraciones

--- src/mapreduce/elespectador.py
+++ src/mapreduce/elespectador.py
@@ -1,5 +1,7 @@
 import hashlib
 import heapq
+import sys
+import traceback
 
 import dumbo
 import redis
@@ -18,7 +20,12 @@ class NoticiaMapper(object):
             html = html.decode('utf-8')
             noticia_str = self.parse(html)
         except Exception:
-            logging.info('could not parse file "%s"' % filename)
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value,
+                                               exc_traceback)
+            # Let's hope redis is not causing the exception :)
+            r.sadd('errores:hadoop',
+                   "Could not parse:'%s'. [%s]" % (filename, ''.join(lines)))
         else:
             noticia = noticia_pb2.Article()
             noticia.ParseFromString(noticia_str)
@@ -46,13 +53,15 @@ class NoticiaMapper(object):
 
 class NoticiaReducer(object):
     def __call__(self, key, match_list):
+        self.key = key
         heap = []
         for match in match_list:
             heapq.heappush(heap,
                            (match[0][0], match[0][1], match[1]))
         for match in self.no_overlaps_iter(heap):
-            yield key, match 
-    
+            if match[2] is not None:
+                yield key, match
+
     def no_overlaps_iter(self, match_list):
         match = heapq.heappop(match_list)
         while True:
@@ -62,8 +71,15 @@ class NoticiaReducer(object):
                 break
             next_match = heapq.heappop(match_list)
             if next_match[0] < match[1]:
-                assert(match[2] == next_match[2])
-                match = (match[0], max(next_match[1], match[1]), match[2])
+                if match[2] != next_match[2]:
+                    r.sadd('errores:hadoop',
+                           'match %s - %s overlap in noticia %s' % (match,
+                                                                    next_match,
+                                                                    self.key))
+                    # Esto se debe a homonimos. Marcar como None ignorar
+                    match = (match[0], max(next_match[1], match[1]), None)
+                else:
+                    match = (match[0], max(next_match[1], match[1]), match[2])
             else:
                 if next_match[0] > match[1]:
                     yield match
