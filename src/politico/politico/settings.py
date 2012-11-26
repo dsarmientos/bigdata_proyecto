2b759e3ce97ade6ab73a0fe156f8291036096fea
Daniel Sarmiento <dsarmientos@gmail.com>
2012-11-25 23:08:14 -0500

    Place holder for front page ready

--- src/politico/politico/settings.py
+++ src/politico/politico/settings.py
@@ -135,6 +135,14 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
@@ -145,7 +153,12 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
     },
     'loggers': {
         'django.request': {
@@ -153,5 +166,10 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
+    'console': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
     }
 }
